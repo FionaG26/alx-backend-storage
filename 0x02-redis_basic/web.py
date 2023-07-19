69ff348680fa3ac59d514dae0fed0623321d1fc4
FionaG26 <fionamuthoni18@gmail.com>
2023-07-19 14:33:47 +0000

    web

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,68 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
-import redis
+
+"""
+This module provides functions for retrieving
+web pages and caching the results.
+"""
+
 import requests
+import time
 from functools import wraps
-from typing import Callable
+from typing import Dict
 
 
-redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
+def cached(expiration_time: int):
+    """
+    Decorator to cache the result of a function with an expiration time.
+
+    Args:
+        expiration_time: The time in seconds until the cache expires.
+
+    Returns:
+        The decorated function.
+    """
+    def decorator(func):
+        cache = {}
+
+        @wraps(func)
+        def wrapper(url):
+            if url in cache and \
+                    time.time() - cache[url]['timestamp'] < expiration_time:
+                cache[url]['count'] += 1
+                return cache[url]['content']
+
+            content = func(url)
+            cache[url] = {
+                'content': content,
+                'timestamp': time.time(),
+                'count': 1
+            }
+            return content
+
+        return wrapper
+
+    return decorator
 
 
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
-    @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
-
-
-@data_cacher
+@cached(expiration_time=10)
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    return requests.get(url).text
+    """
+    Retrieve the HTML content of a web page.
+
+    Args:
+        url: The URL of the web page.
+
+    Returns:
+        The HTML content of the web page as a string.
+    """
+    response = requests.get(url)
+    return response.text
+
+
+if __name__ == '__main__':
+    # Example usage
+    url = ('http://slowwly.robertomurray.co.uk/delay/1000/url/'
+           'http://www.google.co.uk')
+    for _ in range(5):
+        print(get_page(url))
+        time.sleep(2)
