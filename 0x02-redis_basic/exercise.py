2ed325f8c4079827dc022416372c5f7743df7a66
FionaG26 <fionamuthoni18@gmail.com>
2023-07-19 11:49:54 +0000

    1. Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,38 +1,72 @@
 #!/usr/bin/env python3
 
 """
-Main file
+This module provides a Cache class that interacts with Redis
+to store and retrieve data.
 """
 
 import redis
-import uuid
-from typing import Union
+from typing import Callable, Union
 
 
 class Cache:
     """
-    Cache class for storing data in Redis.
+    Cache class for interacting with Redis.
     """
 
-    def __init__(self) -> None:
-        """
-        Initialize the Cache class.
+    def __init__(self):
+        self.redis_client = redis.Redis()
 
-        Creates an instance of the Redis client and flushes the Redis database.
+    def store(self, value: Union[str, bytes, int]) -> str:
         """
-        self._redis = redis.Redis()
-        self._redis.flushdb()
-
-    def store(self, data: Union[str, bytes, int, float]) -> str:
+        Store the value in Redis and return the generated key.
         """
-        Store the input data in Redis using a randomly generated key.
-
-        Args:
-            data: The data to be stored. Can be a str, bytes, int, or float.
-
-        Returns:
-            The randomly generated key used to store the data in Redis.
-        """
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
+        key = str(hash(value))
+        self.redis_client.set(key, value)
         return key
+
+    def get(
+        self, key: str, fn: Callable = None
+    ) -> Union[str, bytes, int, None]:
+        """
+        Retrieve the value from Redis using the given key.
+        If the key does not exist, return None.
+        If a conversion function (fn) is provided, apply
+        it to the retrieved value.
+        """
+        value = self.redis_client.get(key)
+        if value is None:
+            return None
+        if fn is not None:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """
+        Retrieve the value from Redis using the given key and
+        convert it to a string.
+        If the key does not exist, return None.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """
+        Retrieve the value from Redis using the given key and convert
+        it to an integer.
+        If the key does not exist, return None.
+        """
+        return self.get(key, fn=int)
+
+
+if __name__ == "__main__":
+    cache = Cache()
+
+    TEST_CASES = {
+        b"foo": None,
+        123: int,
+        "bar": lambda d: d.decode("utf-8")
+    }
+
+    for value, fn in TEST_CASES.items():
+        key = cache.store(value)
+        assert cache.get(key, fn=fn) == value
